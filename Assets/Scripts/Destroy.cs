8f87f6a9a5a810f9f4ec07a975da17b1face4507
ZacLindsey <zaclindsey1996@pointloma.edu>
2016-10-03 07:44:31 -0700

    first

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="BULLET")
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/rotatorandStuff.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatorandStuff : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere*10.0f;
+    }
+}
